0d734717e63c86f92fce09b46afa15b57f80ceaf
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 15:38:56 +0300

    Rokas

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="css/main.css")
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Rokas 🚀!'
+    return render_template("index.html")
