b5d4152a4c5aae7be6ce6a2488be466ea201352d
Rokas <rookas.rudzenskas@gmail.com>
2021-05-03 15:59:20 +0300

    Rokas

--- main.py
+++ main.py
@@ -5,4 +5,11 @@ app = Flask(__name__, static_url_path="", static_folder="css/main.css")
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+
+    items = [
+        "Cook dinner",
+        "wash Up",
+        "Do laundry",
+        "Clean Room"
+    ]
+    return render_template("index.html", todolist=items)
