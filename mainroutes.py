3aad87eafb67161812ace806a408f94f5e8aac32
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 16:18:18 +0300

    ROkas

--- mainroutes.py
+++ mainroutes.py
@@ -1,6 +1,23 @@
-from flask import Flask, Blueprint, app, render_template
+from flask import Flask, Blueprint, app, render_template, redirect, url_for
 from models import TodoList, TodoItem
+from program import db
+
 routes = Blueprint('routes', __name__)
+
+
+@routes.route('/lists')
+def showLists():
+    lists = TodoList.query.all()
+    return render_template("showlist.html", todolists=lists)
+
+
+@routes.route("/addlist")
+def addList():
+    newList = TodoList()
+    newList.name = "New Todo List"
+    db.session.add(newList)
+    db.session.commit()
+    return redirect(url_for('routes.showLists'))
 
 
 @routes.route('/')
